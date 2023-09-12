9d39943220b05c6739b42f2b82a0205f0223e3f4
busrauz78 <busrauz78@gmail.com>
2023-09-13 01:03:11 +0300

    first commit

---
+++ basics.py
@@ -0,0 +1,48 @@
+print('Hello this is basics file')
+print('This file includes:\nMath Operators\n')
+
+# MATH OPERATORS
+print('MATH OPERATORS:')
+
+# ** : Exponent
+print('Exponent: 2**5:', 2**5)
+
+# % : Modules/Remainder
+print('Remainder of 23 % 7:', 23 % 7)
+
+# // : Integer Division
+print('Integer Division of 23 // 7:' ,23 // 7)
+
+# / : Division
+print('Division of 21 / 2:', 21 / 2)
+
+# * : Multiplication
+print('Division of 5 * 3:', 5 * 3)
+
+# - : Subtraction
+print('Division of 6 - 8:', 6 - 8)
+
+# + : Addition
+print('Division of 7 + 9:', 7 + 9)
+
+print('\n')
+
+# AUGMENTED ASSIGNMENT OPERATORS
+print('AUGMENTED ASSIGNMENT OPERATORS:')
+
+index = 10
+print('Let\'s say index = 10')
+index +=1
+print('index+=1:', index)
+index = 10
+index -=1
+print('index-=1:', index)
+index = 10
+index *=2
+print('index*=2:', index)
+index = 10
+index /=2
+print('index/=2:', index)
+index = 10
+index %=2
+print('index%=2:', index)
